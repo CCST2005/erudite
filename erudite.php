5701635c1e8b3c340b552937742788a5fe1a37bc
assassination0 <125805221+assassination0@users.noreply.github.com>
2023-03-24 22:47:54 +0800

    Add files via upload

---
+++ erudite.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
+
+    <title>Erudite</title>
+
+    <style>
+        <?php
+            include('assets/css/bootstrap.min.css');
+            include('assets/css/font-awesome.css');
+            include('assets/css/templatemo-softy-pinko.css');
+        ?>
+      
+        html {
+        scroll-behavior: smooth;
+        }
+    </style>
+    
+
+    </head>
+    
+    <body>
+    
+
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Welcome Area Start ***** -->
+    <div class="welcome-area" id="welcome">
+
+        <!-- ***** Header Text Start ***** -->
+        <div class="header-text">
+            <div class="container">
+                <div class="row">
+                    <div class="log_in_div">
+                        <img src="assets/logo/logo.svg" alt="">
+                        <a href="login.html"><button>LOG IN</button></a>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        <!-- ***** Header Text End ***** -->
+    </div>
+    <!-- ***** Welcome Area End ***** -->
+
+   
+    <!-- ***** Features Small Start ***** -->
+    <section class="section home-feature">
+        <div class="container ads_row">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <!-- ***** Features Small Item Start ***** -->
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12 another_div_class" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
+                            <div class="features-small-item">
+                                <div class="bodies">
+                                    <div class="icon">
+                                        <i><img src="assets/icon_landing_page/library.svg" alt=""></i>
+                                    </div>
+                                
+                                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The pointetters, as</p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <!-- ***** Features Small Item End ***** -->
+
+                        <!-- ***** Features Small Item Start ***** -->
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12 another_div_class" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s">
+                            <div class="features-small-item">
+                            <div class="bodies">
+                                    <div class="icon">
+                                    <i><img src="assets/icon_landing_page/openbook.svg" alt=""></i>
+                                    </div>
+                                
+                                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The pointetters, as</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- ***** Features Small Item End ***** -->
+
+                        <!-- ***** Features Small Item Start ***** -->
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12 another_div_class" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.6s">
+                            <div class="features-small-item">
+                            <div class="bodies">
+                                    <div class="icon">
+                                    <i><img src="assets/icon_landing_page/librarian.svg" alt=""></i>
+                                    </div>
+                                
+                                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The pointetters, as</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- ***** Features Small Item End ***** -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Small End ***** -->
+
+    <!-- ***** Features Big Item Start ***** -->
+    <section class="section padding-top-70 padding-bottom-0" id="features">
+        <hr class="line-above-book">
+        <h1 class="books-header" style="text-align:center;color:#25316D">BOOKS</h1>
+        
+        <div class="books-con container">
+
+            <div class="row row-book row-right line-row">
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
+                    <div class="left-heading">
+                        <h2 class="right-sec section-title">Let's discuss about you project</h2>
+                    </div>
+                    <div class="right-sec left-text">
+                        <p>Nullam sit amet purus libero. Etiam ullamcorper nisl ut augue blandit, at finibus leo efficitur. Nam gravida purus non sapien auctor, ut aliquam magna ullamcorper.</p>
+                        <button class="read-more-button">Read more</button>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <img src="book.webp" class="rounded img-fluid d-block mx-auto" alt="App">
+                    
+                </div>
+            </div>
+
+
+
+            <div class="row row-book line-row">
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <img src="book.webp" class="rounded img-fluid d-block mx-auto" alt="App">
+                </div>
+                
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
+                    <div class="left-heading">
+                        <h2 class="section-title">Let’s discuss about you project</h2>
+                    </div>
+                    <div class="left-text">
+                        <p>Nullam sit amet purus libero. Etiam ullamcorper nisl ut augue blandit, at finibus leo efficitur. Nam gravida purus non sapien auctor, ut aliquam magna ullamcorper.</p>
+                        <button class="read-more-button">Read more</button>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row row-book row-right line-row">
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
+                    <div class="left-heading">
+                        <h2 class="right-sec section-title">Let’s discuss about you project</h2>
+                    </div>
+                    <div class="right-sec left-text">
+                        <p>Nullam sit amet purus libero. Etiam ullamcorper nisl ut augue blandit, at finibus leo efficitur. Nam gravida purus non sapien auctor, ut aliquam magna ullamcorper.</p>
+                        <button class="read-more-button">Read more</button>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <img src="book.webp" class="rounded img-fluid d-block mx-auto" alt="App">
+                    
+                </div>
+            </div>
+
+            <div class="row row-book line-row">
+                <div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <img src="book.webp" class="rounded img-fluid d-block mx-auto" alt="App">
+                </div>
+                
+                <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
+                    <div class="left-heading">
+                        <h2 class="section-title">Let’s discuss about you project</h2>
+                    </div>
+                    <div class="left-text">
+                        <p>Nullam sit amet purus libero. Etiam ulkikkkkkkkkkkkkkkk kkkkkkkkkklamcorper nisl ut augue blandit, at finibus leo efficitur. Nam gravida purus non sapien auctor, ut aliquam magna ullamcorper.</p>
+                        <button class="read-more-button">Read more</button>
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+
+        </div>
+  
+        <div>
+                <div class="see-more-divs">
+                    <a href="" class="see-see">See more</a>
+                </div>
+            </div>
+       
+    </section>
+   
+    <section class="section colored" id="pricing-plans">
+        <div class="container">
+            <!-- ***** Section Title Start ***** -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="center-heading" >
+                        <h1 class="books-header" style="margin-bottom:70px;margin-top:0;text-align:center;color:#F2EAAB">ABOUT US</h1>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="center-text" style="width:100%">
+                        <p style="color:#F2EAAB;font-size:20px">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae inventore at aperiam culpa perspiciatis in molestiae ullam nulla deleniti aspernatur, consequuntur reprehenderit alias iure mollitia sed, odit maiores corrupti a impedit nam facere quasi animi sequi. Sit obcaecati officia eligendi dolorum placeat, aliquid molestias doloremque quo consequuntur vero qui hic provident accusamus illum laborum. Vel non, nostrum nam eos magni est molestias. Nemo et at laboriosam quisquam! Unde, aliquam nulla numquam eveniet mollitia ea, quasi ab facilis consequuntur iste repellendus ullam alias similique ducimus inventore tempore ipsam omnis! Enim ex inventore et earum blanditiis cum quam. Voluptates eos rem excepturi repellat fuga quibusdam eum modi dolores rerum? Placeat et commodi earum veritatis, similique quibusdam in eius velit tempora provident. Laborum asperiores accusantium omnis. Dicta laudantium quas quidem magni accusantium vel deserunt sunt eos, voluptas veritatis vitae eum voluptatum, minus, ipsum atque? Ipsam cum est, minima qui vel numquam distinctio fugit inventore ipsum soluta cupiditate, aut laboriosam nesciunt maxime et expedita. Sint rem vitae, magnam, doloremque numquam, reprehenderit voluptatum sequi deleniti qui consequatur praesentium. Magnam, facere esse minima ab repellat deleniti perferendis totam et enim excepturi ad. Omnis quos id adipisci qui magni cumque vitae cupiditate, sequi deleniti quas cum sapiente deserunt numquam minima earum ab veritatis necessitatibus consectetur quibusdam suscipit? Doloribus totam quia omnis maxime tempore quos at non sunt vero possimus neque error ea excepturi aliquam ipsum in deleniti, ipsa amet eum accusamus officia eveniet. Quod tempore rerum facere labore. Enim aliquid et quae quibusdam voluptate odit necessitatibus accusamus numquam aperiam! Obcaecati, sed et molestiae tempora officiis nihil eaque odit provident distinctio asperiores dolorem iusto sit doloremque quisquam ipsum ipsa culpa animi assumenda, itaque ut, facere minima tempore perspiciatis? Perspiciatis sequi odio, possimus, delectus necessitatibus, blanditiis neque fugit eos quo pariatur dolorum reiciendis asperiores reprehenderit animi iusto odit illo sint aliquam dolor tempora libero magnam quod! Accusantium incidunt hic consequatur quaerat perspiciatis corporis est cupiditate eum in, quidem, voluptate adipisci fuga accusamus commodi eos possimus ad pariatur quibusdam sapiente a deleniti illo quo perferendis recusandae? Consectetur ipsum veritatis aliquam culpa magnam, deleniti iure sunt!
+                    </p>
+                    </div>
+                </div>
+            </div>
+            <!-- ***** Section Title End ***** -->
+
+            
+        </div>
+    </section>
+
+
+    <!-- ***** Blog Start ***** -->
+    <section class="" id="" id="signupSec">
+        <div class="container_signup">
+            <div class="sign-up">
+                <div class="div-sign">
+                    <div class="logo-sign">
+                        <img src="logo-ccst.svg" alt="">
+                    </div>
+                    <div class="logo-sign remove-sign">
+                        <h1 class="sign-hOne" style="text-align:center;color:#25316D">SIGN IN</h1>
+                        <p style="text-align:center;color:#25316D">False information may lead to rejection.</p>
+                        <div class="div-info">
+                            <div class="div-all">
+                                <div class="div-input">
+                                    <div class="div-name">
+                                        <span>First name</span>
+                                        <input type="text">
+                                    </div>
+                                    <div class="div-name">
+                                        <span>Middle name</span>
+                                        <input type="text">
+                                    </div>
+                                </div>
+                                
+                                <div class="div-input">
+                                    <div class="div-name">
+                                        <span>Last name</span>
+                                        <input type="text">
+                                    </div>
+                                    <div class="div-name">
+                                        <span>Student number</span>
+                                        <input type="text">
+                                    </div>
+                                </div>
+                                    <script>
+                                        function showPass(){
+                                            if (show.checked == true){
+                                                passwords.type = 'text';
+                                                passwordC.type = 'text';
+                                            }
+                                            else{
+                                                passwords.type = 'password';
+                                                passwordC.type = 'password';
+                                            }
+                                        }
+                                    </script>
+                                <div class="div-input">
+                                    <div class="div-name">
+                                        <span>Password</span>
+                                        <input id="passwords" type="password">
+                                    </div>
+
+
+                                    <div class="check-div">
+                                        <div class="div-name">
+                                            <span>Confirm password</span>
+                                            <input id="passwordC" type="password">
+                                        </div>
+                                        <div class="check-box">
+                                      
+                                           <input onclick="showPass()" type="checkbox" name="" id="show"><span>Show</span> 
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="div-input">
+                                    <div class="select-div">
+                                        <select name="" id="">
+                                            <option value="">Grade</option>
+                                            <option value="">dsadsad</option>
+                                        </select>
+                                  
+                                   
+                                        <select name="" id="">
+                                            <option value="">Section</option>
+                                            <option value="">dsadsad</option>
+                                        </select>
+                                    </div>
+                                    <div class="div-name">
+                                        <span>I.D</span>
+                                        <input id="img" type="file">
+                                        <label class="file-img" for="img">Select image file</label>
+                                    </div>
+                                </div>
+                                <div class="div-input">
+                                    <div class="only">
+                                        <p>Only Clarkians can send a request.</p><button>Send</button>
+                                    </div>
+                                </div>
+                              
+                                  
+                                  
+                                </div>
+                                
+                                
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Blog End ***** -->
+
+    <!-- ***** Contact Us Start ***** -->
+    
+    <!-- ***** Contact Us End ***** -->
+    
+    <!-- ***** Footer Start ***** -->
+    <footer>
+       
+        
+    </footer>
+    
+    <!-- jQuery -->
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
+
+    <title>Erudite</title>
+
+    <style>
+        <?php
+            include('assets/css/bootstrap.min.css');
+            include('assets/css/font-awesome.css');
+            include('assets/css/templatemo-softy-pinko.css');
+        ?>
+      
+        html {
+        scroll-behavior: smooth;
+        }
+    </style>
+    
+
+    </head>
+    
+    <body>
+    
+
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Welcome Area Start ***** -->
+    
+    <!-- ***** Welcome Area End ***** -->
+
+   
+    <!-- ***** Features Small Start ***** -->
+    
+    <!-- ***** Features Small End ***** -->
+
+    <!-- ***** Features Big Item Start ***** -->
+    
+   
+    
+
+
+    <!-- ***** Blog Start ***** -->
+    <section class="" id="" id="signupSec">
+        <div class="container_signup">
+            <div class="sign-up">
+                <div class="div-sign">
+                    <div class="logo-sign">
+                        <img src="logo-ccst.svg" alt="">
+                    </div>
+                    <div class="logo-sign remove-sign">
+                        <h1 class="sign-hOne" style="text-align:center;color:#25316D">SIGN IN</h1>
+                        <p style="text-align:center;color:#25316D">False information may lead to rejection.</p>
+                        <div class="div-info">
+                            <div class="div-all">
+                                <div class="div-input">
+                                    <div class="div-name">
+                                        <span>First name</span>
+                                        <input type="text">
+                                    </div>
+                                    <div class="div-name">
+                                        <span>Middle name</span>
+                                        <input type="text">
+                                    </div>
+                                </div>
+                                
+                                <div class="div-input">
+                                    <div class="div-name">
+                                        <span>Last name</span>
+                                        <input type="text">
+                                    </div>
+                                    <div class="div-name">
+                                        <span>Student number</span>
+                                        <input type="text">
+                                    </div>
+                                </div>
+                                    <script>
+                                        function showPass(){
+                                            if (show.checked == true){
+                                                passwords.type = 'text';
+                                                passwordC.type = 'text';
+                                            }
+                                            else{
+                                                passwords.type = 'password';
+                                                passwordC.type = 'password';
+                                            }
+                                        }
+                                    </script>
+                                <div class="div-input">
+                                    <div class="div-name">
+                                        <span>Password</span>
+                                        <input id="passwords" type="password">
+                                    </div>
+
+
+                                    <div class="check-div">
+                                        <div class="div-name">
+                                            <span>Confirm password</span>
+                                            <input id="passwordC" type="password">
+                                        </div>
+                                        <div class="check-box">
+                                      
+                                           <input onclick="showPass()" type="checkbox" name="" id="show"><span>Show</span> 
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="div-input">
+                                    <div class="select-div">
+                                        <select name="" id="">
+                                            <option value="">Grade</option>
+                                            <option value="">dsadsad</option>
+                                        </select>
+                                  
+                                   
+                                        <select name="" id="">
+                                            <option value="">Section</option>
+                                            <option value="">dsadsad</option>
+                                        </select>
+                                    </div>
+                                    <div class="div-name">
+                                        <span>I.D</span>
+                                        <input id="img" type="file">
+                                        <label class="file-img" for="img">Select image file</label>
+                                    </div>
+                                </div>
+                                <div class="div-input">
+                                    <div class="only">
+                                        <p>Only Clarkians can send a request.</p><button>Send</button>
+                                    </div>
+                                </div>
+                              
+                                  
+                                  
+                                </div>
+                                
+                                
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="assets/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/popper.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/js/scrollreveal.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/imgfix.min.js"></script> 
+    
+    <!-- Global Init -->
+    <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
